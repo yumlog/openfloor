2ec5c7db4df7ead8ae1b299fc327b6960b9fdb63
yumlog <yumlog314@gmail.com>
2025-01-14 19:36:12 +0900

    스크롤 패럴랙스 효과 적용

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -5,10 +5,13 @@ import { useRef, useState, useEffect } from "react";
 import styles from "./main.module.scss";
 import classNames from "classnames/bind";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import TestIcon from "/public/images/window.svg";
-import LogoBackground from "/public/images/logo-bg.svg";
 import Tilt from "react-parallax-tilt";
 import Image from "next/image";
+import TestIcon from "/public/images/window.svg";
+import LinkIcon from "/public/images/icon-48-link.svg";
+import DownloadIcon from "/public/images/icon-40-download.svg";
+import LogoBackground from "/public/images/logo-bg.svg";
+import Logo from "/public/images/logo.svg";
 
 const cx = classNames.bind(styles);
 
@@ -93,100 +96,109 @@ export default function Main() {
         smooth: true,
         smoothMobile: true,
         lerp: 0.02,
+        direction: "vertical",
       }}
       watch={[]}
       containerRef={containerRef}
     >
-      <main data-scroll-container ref={containerRef}>
+      <main data-scroll-container ref={containerRef} className={cx("main")}>
         <div className={cx("custom-cursor")} ref={cursorRef} />
         <div className={cx("logo-bg")}>
           <LogoBackground />
         </div>
         <section data-scroll-section className={cx("section1")}>
-          <motion.div {...motionProps}>
-            <TestIcon width={160} height={142} />
-            <h1>Openfloor Makes Quality</h1>
-            <p>최고의 가치를 오픈플로어와 함께 합니다.</p>
-          </motion.div>
+          <div data-scroll data-scroll-speed="1">
+            <motion.div {...motionProps}>
+              <Logo width={160} height={142} />
+              <h1>Openfloor Makes Quality</h1>
+              <p>최고의 가치를 오픈플로어와 함께 합니다.</p>
+            </motion.div>
+          </div>
         </section>
         <section data-scroll-section className={cx("section2")}>
-          <motion.div {...motionProps}>
-            <h1>
-              Service
-              <br />
-              Philosophy
-            </h1>
-          </motion.div>
-          <motion.div {...motionProps}>
-            <ul className={cx("box-list")}>
-              <li>
-                <TestIcon width={32} height={32} />
-                <p>Efficiency</p>
-                <ul className={cx("dot-list")}>
-                  <li>불필요한 개발 과정 축소</li>
-                  <li>결과를 극대화할 수 있는 리소스 보유</li>
-                </ul>
-              </li>
-              <li>
-                <TestIcon width={32} height={32} />
-                <p>Technology</p>
-                <ul className={cx("dot-list")}>
-                  <li>트랜드에 맞는 최신 기술력 보유</li>
-                  <li>AI, 디지털 트윈 등 IT 트렌드 선도</li>
-                </ul>
-              </li>
-              <li>
-                <TestIcon width={32} height={32} />
-                <p>Quality</p>
-                <ul className={cx("dot-list")}>
-                  <li>다양한 경험을 통한 깔끔한 코드</li>
-                  <li>파트별 검증 과정을 통한 높은 결과물</li>
-                </ul>
-              </li>
-              <li>
-                <button className={cx("hover-target")}>클릭</button>
-                <TestIcon width={32} height={32} />
-                <p>Contact</p>
-                <ul className={cx("dot-list")}>
-                  <li>고객 맞춤형 상담과 신속한 피드백</li>
-                  <li>원활한 소통으로 요구사항 파악</li>
-                </ul>
-              </li>
-            </ul>
-          </motion.div>
+          <div data-scroll data-scroll-speed="-1">
+            <motion.div {...motionProps}>
+              <h1>
+                Service
+                <br />
+                Philosophy
+              </h1>
+            </motion.div>
+            <motion.div {...motionProps}>
+              <ul className={cx("box-list")}>
+                <li>
+                  <TestIcon width={32} height={32} />
+                  <p>Efficiency</p>
+                  <ul className={cx("dot-list")}>
+                    <li>불필요한 개발 과정 축소</li>
+                    <li>결과를 극대화할 수 있는 리소스 보유</li>
+                  </ul>
+                </li>
+                <li>
+                  <TestIcon width={32} height={32} />
+                  <p>Technology</p>
+                  <ul className={cx("dot-list")}>
+                    <li>트랜드에 맞는 최신 기술력 보유</li>
+                    <li>AI, 디지털 트윈 등 IT 트렌드 선도</li>
+                  </ul>
+                </li>
+                <li>
+                  <TestIcon width={32} height={32} />
+                  <p>Quality</p>
+                  <ul className={cx("dot-list")}>
+                    <li>다양한 경험을 통한 깔끔한 코드</li>
+                    <li>파트별 검증 과정을 통한 높은 결과물</li>
+                  </ul>
+                </li>
+                <li>
+                  <button className={cx("hover-target")}>
+                    <LinkIcon width={48} height={48} />
+                  </button>
+                  <TestIcon width={32} height={32} />
+                  <p>Contact</p>
+                  <ul className={cx("dot-list")}>
+                    <li>고객 맞춤형 상담과 신속한 피드백</li>
+                    <li>원활한 소통으로 요구사항 파악</li>
+                  </ul>
+                </li>
+              </ul>
+            </motion.div>
+          </div>
         </section>
         <section data-scroll-section className={cx("section3")}>
-          <motion.div {...motionProps}>
-            <h1>Digital Marketing Counsulting</h1>
-          </motion.div>
-          <motion.div {...motionProps}>
-            <ul className={cx("box-list")}>
-              <li>
-                <div></div>
-                <p>Consulting</p>
-                <ul className={cx("dot-list")}>
-                  <li>시장 분석 기반 맞춤형 마케팅 전략</li>
-                  <li>체계적인 성과 개선과 비즈니스 성장을 지원</li>
-                </ul>
-              </li>
-              <li>
-                <div></div>
-                <p>Data Marketing</p>
-                <ul className={cx("dot-list")}>
-                  <li>PPC·SNS 광고와 A/B 테스트로 성과 최적화</li>
-                  <li>ROI를 극대화하는 정교한 마케팅을 제공</li>
-                </ul>
-              </li>
-              <li>
-                <div></div>
-                <p>SEO</p>
-                <ul className={cx("dot-list")}>
-                  <li>SEO 콘텐츠로 가시성 및 고객 유입 증대</li>
-                  <li>가치 있는 콘텐츠로 고객과 신뢰를 구축</li>
-                </ul>
-              </li>
-            </ul>
-          </motion.div>
+          <div data-scroll data-scroll-speed="-1">
+            <motion.div {...motionProps}>
+              <h1>Digital Marketing Counsulting</h1>
+            </motion.div>
+            <motion.div {...motionProps}>
+              <ul className={cx("box-list")}>
+                <li data-scroll data-scroll-speed="0">
+                  <div></div>
+                  <p>Consulting</p>
+                  <ul className={cx("dot-list")}>
+                    <li>시장 분석 기반 맞춤형 마케팅 전략</li>
+                    <li>체계적인 성과 개선과 비즈니스 성장을 지원</li>
+                  </ul>
+                </li>
+                <li data-scroll data-scroll-speed="1">
+                  <div></div>
+                  <p>Data Marketing</p>
+                  <ul className={cx("dot-list")}>
+                    <li>PPC·SNS 광고와 A/B 테스트로 성과 최적화</li>
+                    <li>ROI를 극대화하는 정교한 마케팅을 제공</li>
+                  </ul>
+                </li>
+                <li data-scroll data-scroll-speed="2">
+                  <div></div>
+                  <p>SEO</p>
+                  <ul className={cx("dot-list")}>
+                    <li>SEO 콘텐츠로 가시성 및 고객 유입 증대</li>
+                    <li>가치 있는 콘텐츠로 고객과 신뢰를 구축</li>
+                  </ul>
+                </li>
+              </ul>
+            </motion.div>
+          </div>
         </section>
         <section data-scroll-section className={cx("section4")}>
           <motion.div {...motionProps}>
@@ -246,7 +258,7 @@ export default function Main() {
                 지향합니다.
               </p>
               <button className={cx("hover-target")}>
-                <TestIcon width={40} height={40} />
+                <DownloadIcon width={40} height={40} />
                 회사소개서 다운로드
               </button>
             </div>
@@ -321,6 +333,14 @@ export default function Main() {
               </span>
             </motion.div>
           </div>
+          <ul className={cx("test")}>
+            <li data-scroll data-scroll-speed="2">
+              느려
+            </li>
+            <li data-scroll data-scroll-speed="-2">
+              빨라
+            </li>
+          </ul>
         </section> */}
         <div data-scroll-section className={cx("footer")}>
           OPENFLOOR
